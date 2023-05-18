abcaaa4473cae1e2d9f485e2e91cf1e1ce774a24
Sergio Utrera <sergiactive@gmail.com>
2023-05-18 13:14:44 +0100

    Added header nav

---
+++ template-parts/header/nav.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Header Navigation Template
+ * 
+ * @package WPixel
+ */
+
+ ?>
+<a class="navbar-brand d-block d-sm-block d-md-none text-center pt-4" href="index.html">
+	<img src="images/logos/CineclubblancoCortoP.svg" alt="" width="65" height="auto">
+</a>
+<nav class="navbar navbar-expand-md navbar-dark d-none d-md-block">
+		<div class="container">
+			<a class="navbar-brand d-none d-md-block me-5" href="index.html">
+				<img src="images/logos/CineclubblancoLargocompleto.svg" alt="" width="155" height="auto">
+			</a>
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item dropdown">
+						<a class="nav-link active dropdown-toggle" aria-current="page" id="navbarDropdownMenuLink" role="button"
+							href="#" data-bs-toggle="dropdown" aria-expanded="false">Cineclub VO
+							35</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+							<li><a class="dropdown-item" href="/teatro-apolo.html">Teatro Apolo</a></li>
+							<li><a class="dropdown-item" href="/ciclos-anteriores.html">Ciclos anteriores</a></li>
+							<li><a class="dropdown-item" href="/quienes-somos.html">Quienes somos</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/programacion-educativa.html">Programas educativos</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/contacto.html">Contacto</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="/blog.html">Blog</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
