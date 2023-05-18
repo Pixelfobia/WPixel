f2d4483f22876d491ab3a2776c1f1bb6d688a7cc
Sergio Utrera <sergiactive@gmail.com>
2023-05-18 11:51:20 +0100

    Added CSS and JS Skeleton

--- index.php
+++ index.php
@@ -5,17 +5,12 @@
  * @package WPixel
  */
 
-
+get_header();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>WPixel</title>
-</head>
-<body>
+
+	<div class="content">
+		Content
+	</div>
 	
-</body>
-</html>
+	<?php 
+	get_footer(); 
