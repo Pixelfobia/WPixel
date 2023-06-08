60d7ae5bee83691d313d3dc2aae6bc7e88ed36b5
Sergio Utrera <sergiactive@gmail.com>
2023-06-08 17:12:26 +0100

    Added Blocks instance

--- inc/classes/class-wpixel-theme.php
+++ inc/classes/class-wpixel-theme.php
@@ -19,8 +19,8 @@ class WPIXEL_THEME {
 		Menus::get_instance();
 		Meta_Boxes::get_instance();
 		Sidebars::get_instance();
+		Blocks::get_instance();
 		Block_Patterns::get_instance();
-		// Block_Patterns::get_instance();
 		// Loadmore_Posts::get_instance();
 		// Loadmore_Single::get_instance();
 		// Register_Post_Types::get_instance();
@@ -197,7 +197,7 @@ class WPIXEL_THEME {
 		 */
 		global $content_width;
 		if ( ! isset( $content_width ) ) {
-			$content_width = 1400;
+			$content_width = 1240;
 		}
 	}
 
